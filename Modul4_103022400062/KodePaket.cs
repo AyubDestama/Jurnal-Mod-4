5f7ea253161384d210ee3228f30b1ccf336a69e2
Tamagochi <ayubdestama@gmail.com>
2026-03-28 14:23:58 +0700

    second commit

---
+++ Modul4_103022400062/KodePaket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400062
+{
+    public class KodePaket
+    {
+        public string getKodePaket(string namaPaket)
+        {
+            switch (namaPaket.ToLower())
+            {
+                case "basic": return "P201";
+                case "standard": return "P202";
+                case "premium": return "P203";
+                case "unlimited": return "P204";
+                case "gaming": return "P205";
+                case "streaming": return "P206";
+                case "family": return "P207";
+                case "business": return "P208";
+                case "student": return "P209";
+                case "traveler": return "P210";
+                default: return "Kode tidak ditemukan";
+            }
+        }
+    }
+}
--- Modul4_103022400062/Program.cs
+++ Modul4_103022400062/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Modul4_103022400062;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePaket kp = new KodePaket();
+        Console.WriteLine("Masukkan nama Paket: ");
+
+        string nama = Console.ReadLine();
+        string kode = kp.getKodePaket(nama);
+
+        Console.WriteLine("Kode Paket: " + kode);
+    }
+}
